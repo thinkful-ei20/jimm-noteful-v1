cef3a87e83a635a89036474d8423f399be1efa41
unknown <ajameslusk@gmail.com>
2018-04-18 15:44:36 -0400

    added delete note functionality to client and server

--- router/notes.router.js
+++ router/notes.router.js
@@ -46,10 +46,45 @@ router.put('/notes/:id', (req, res, next) => {
       return next(err);
     }
     if (item) {
-      res.json(item);
+      res.status(201).json(item);
     } else {
       next();
     }
+  });
+});
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.delete('/notes/:id', (req, res, next) => {
+  let {id} = req.params;
+  notes.delete(id, (err) => {
+    if(err){
+      err.status = 500;
+      return next(err);
+    }
+    res.status(204).end();
   });
 });
 
